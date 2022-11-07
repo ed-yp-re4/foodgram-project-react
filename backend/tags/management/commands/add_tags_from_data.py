9a3abba869bd81ea4a5025d96cc00ee89a425d03
Roman Cherepanov <r.e.cherepanov@gmail.com>
2022-11-07 22:52:07 +0300

    dev

---
+++ backend/tags/management/commands/add_tags_from_data.py
@@ -0,0 +1,28 @@
+import csv
+import os
+
+from django.core.management.base import BaseCommand
+
+from foodgram_project.settings import BASE_DIR
+from tags.models import Tag
+
+
+class Command(BaseCommand):
+    help = 'Копирование данных из csv'
+    shift_path = os.path.join(BASE_DIR, 'start_data')
+
+    def handle(self, *args, **kwargs):
+
+        print(self.shift_path)
+        filename = os.path.join(self.shift_path, 'tags.csv')
+        with open(filename, 'r', encoding='utf-8') as f:
+            csv_reader = csv.reader(f, delimiter=',', quotechar='"')
+            for row in csv_reader:
+                name = row[0]
+                color = row[1]
+                slug = row[2]
+                tag = Tag.objects.create(
+                    name=name, color=color, slug=slug
+                )
+                print(tag)
+        print('ADD', Tag.objects.count(), 'tags')
---
+++ backend/tags/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+class Tag(models.Model):
+
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название',
+        help_text='Название',
+        unique=True,
+    )
+    color = models.CharField(
+        max_length=7,
+        verbose_name='Цвет',
+        help_text='Цвет',
+        unique=True
+    )
+    slug = models.SlugField(
+        max_length=200,
+        verbose_name='Slug',
+        help_text='Slug',
+        unique=True,
+    )
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+        ordering = ('slug',)
+
+    def __str__(self):
+        return f'Tag: {self.slug}-{self.color}'
+
+    def __repr__(self):
+        return f'Tag: {self.slug}-{self.color}'
