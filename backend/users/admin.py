b92666929f3479d90a03e6ca0e1c81abde5338be
Roman Cherepanov <r.e.cherepanov@gmail.com>
2022-11-07 22:38:55 +0300

    dev

---
+++ backend/users/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from users.models import SubscribeUser, User
+
+
+class SubscribeUserAdmin(admin.ModelAdmin):
+
+    list_display = ('pk', 'user', 'author',)
+    list_editable = ('user', 'author',)
+    search_fields = ('user__username', 'author__username')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(SubscribeUser, SubscribeUserAdmin)
