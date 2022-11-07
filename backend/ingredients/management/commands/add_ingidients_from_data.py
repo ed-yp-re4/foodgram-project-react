7be9e968b5d062e4e71a5f578c999fb70270b752
Roman Cherepanov <r.e.cherepanov@gmail.com>
2022-11-07 22:46:17 +0300

    dev

---
+++ backend/ingredients/management/commands/add_ingidients_from_data.py
@@ -0,0 +1,31 @@
+import csv
+import os
+
+from django.core.management.base import BaseCommand
+from foodgram_project.settings import BASE_DIR
+from ingredients.models import Ingredient, MeasurementUnit
+
+
+class Command(BaseCommand):
+    help = 'Копирование данных из csv'
+
+    shift_path = os.path.join(BASE_DIR, 'start_data')
+
+    def handle(self, *args, **kwargs):
+
+        print(self.shift_path)
+        filename = os.path.join(self.shift_path, 'ingredients.csv')
+        with open(filename, 'r', encoding='utf-8') as f:
+            csv_reader = csv.reader(f, delimiter=',', quotechar='"')
+            for row in csv_reader:
+                unit_data = row[1].strip().lower()
+                ingrid_data = row[0].strip().lower()
+                unit, _ = MeasurementUnit.objects.get_or_create(
+                    name=unit_data
+                )
+                Ingredient.objects.create(
+                    name=ingrid_data, measurement_unit=unit
+                )
+
+        print('ADD', MeasurementUnit.objects.count(), 'MeasurementUnits')
+        print('ADD', Ingredient.objects.count(), 'Ingredients')
---
+++ backend/ingredients/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+
+class MeasurementUnit(models.Model):
+    '''Размерность игридиентов.'''
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название',
+        help_text='Название',
+        unique=True,
+    )
+
+    class Meta:
+        verbose_name = 'Размерность'
+        verbose_name_plural = 'Размерности'
+        ordering = ('name',)
+
+    def __str__(self):
+        return f'Размерность: {self.name}'
+
+    def __repr__(self):
+        return f'Размерность: {self.name}'
+
+
+class Ingredient(models.Model):
+    '''Модель ингридиентов.'''
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название',
+        help_text='Название',
+    )
+    measurement_unit = models.ForeignKey(
+        MeasurementUnit,
+        on_delete=models.CASCADE,
+        null=True,
+        related_name='ingredients',
+        verbose_name='Размерность',
+        help_text='Размерность',
+        db_index=True,
+    )
+
+    class Meta:
+        verbose_name = 'Ингридиент'
+        verbose_name_plural = 'Ингридиенты'
+        ordering = ('name',)
+
+    def __str__(self) -> str:
+        return f'Ингридиент: {self.name}'
+
+    def __repr__(self) -> str:
+        return f'Ингридиент: {self.name}'
